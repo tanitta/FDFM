0fd03c4472677dbcd11bbeae3761d5a88a8f9db5
tanitta <ttata.trit@gmail.com>
2013-12-23 20:56:35 +0900

    既存のArmを移植開始

--- src/main/scala/BaseArm.scala
+++ src/main/scala/BaseArm.scala
@@ -2,4 +2,17 @@ package trit.fdfm.arms
 
 class BaseArm(var degreeOfFreedom : Int){
 	var angle = new Array[Double](degreeOfFreedom)
+	def FK(){}
+	def IK(){}
+}
+
+class Arm1(var degreeOfFreedom : Int) extends BaseArm {
+	var angle = new Array[Double](degreeOfFreedom)
+	override def FK(theta1:Double, theta2:Double, theta3:Double, theta4:Double){
+		
+	}
+	
+	override def IK(Xe:Double,Ye:Double,Ze:Double,Xh:Double,Yh:Double,Zh:Double){
+		
+	}
 }
