4b0716c55958163b7979acac6c2d1c1a786c26ef
tanitta <ttata.trit@gmail.com>
2013-12-24 21:54:18 +0900

    同次変換行列の宣言

--- src/main/scala/BaseArm.scala
+++ src/main/scala/BaseArm.scala
@@ -8,6 +8,17 @@ class BaseArm(var degreeOfFreedom : Int){
 
 class Arm1(var degreeOfFreedom : Int) extends BaseArm {
 	var angle = new Array[Double](degreeOfFreedom)
+	
+	var Td0 = new Jama.Matrix(4,4)
+	var T01 = new Jama.Matrix(4,4)
+	var T12 = new Jama.Matrix(4,4)
+	var T23 = new Jama.Matrix(4,4)
+	var T34 = new Jama.Matrix(4,4)
+	
+	var T02 = new Jama.Matrix(4,4)
+	var T03 = new Jama.Matrix(4,4)
+	var T04 = new Jama.Matrix(4,4)
+	
 	override def FK(theta1:Double, theta2:Double, theta3:Double, theta4:Double){
 		
 	}
